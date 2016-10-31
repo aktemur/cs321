731c847d4acd4936b30f2d24314bec12124236a1
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2016-10-31 14:36:17 +0300

    Representing boolean values using real bools, not simply 0 and 1.

--- SimpleExpr/simpleExpr.ml
+++ SimpleExpr/simpleExpr.ml
@@ -4,34 +4,37 @@ type expr = CstI of int
           | Let of string * expr * expr
           | If of expr * expr * expr
 
+type value = Int of int
+           | Bool of bool
+
 let rec lookup x env =
   match env with
   | [] -> failwith ("Name '" ^ x ^ "' not found :(")
   | (y,i)::rest -> if y = x then i
                    else lookup x rest
               
-(* eval: expr -> (string * int) list -> int *)
+(* eval: expr -> (string * int) list -> value *)
 let rec eval e env =
   match e with
-  | CstI i -> i
+  | CstI i -> Int(i)
   | Var x -> lookup x env
   | Prim(op, e1, e2) ->
-     let (i1, i2) = (eval e1 env, eval e2 env) in
-     (match op with
-      | "+" -> i1 + i2
-      | "-" -> i1 - i2
-      | "*" -> i1 * i2
-      | "/" -> i1 / i2
-      | "=" -> if i1 = i2 then 1 else 0
-      | "<" -> if i1 < i2 then 1 else 0
-      | _ -> failwith "Unrecognized Prim operator"
+     let (v1, v2) = (eval e1 env, eval e2 env) in
+     (match op, v1, v2 with
+      | "+", Int(i1), Int(i2) -> Int(i1 + i2)
+      | "-", Int(i1), Int(i2) -> Int(i1 - i2)
+      | "*", Int(i1), Int(i2) -> Int(i1 * i2)
+      | "/", Int(i1), Int(i2) -> Int(i1 / i2)
+      | "=", Int(i1), Int(i2) -> if i1 = i2 then Bool(true) else Bool(false)
+      | "<", Int(i1), Int(i2) -> if i1 < i2 then Bool(true) else Bool(false)
+      | _ -> failwith "Unrecognized Prim operator or bad values."
      )
   | Let(x, e1, e2) -> let i = eval e1 env in
                       let newEnv = (x, i)::env in
                       eval e2 newEnv
   | If(c, e1, e2) -> (match eval c env with
-                      | 1 -> eval e1 env
-                      | 0 -> eval e2 env
+                      | Bool(true) -> eval e1 env
+                      | Bool(false) -> eval e2 env
                       | _ -> failwith "WTF!")
 
 let e1 = Prim("-", Prim("+", CstI 4, Var "a"), CstI 5)
