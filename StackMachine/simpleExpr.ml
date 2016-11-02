05a81d65580272a3a3b09f8b51ca684064a1c460
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2016-11-02 11:33:51 +0300

    Using "Prim" constructor.

--- StackMachine/simpleExpr.ml
+++ StackMachine/simpleExpr.ml
@@ -1,49 +1,55 @@
 type expr = CstI of int
-          | Plus of expr * expr
-          | Star of expr * expr
-          | Minus of expr * expr
-          | Slash of expr * expr
+          | Prim of string * expr * expr
 
 (* eval: expr -> int *)
 let rec eval e =
   match e with
   | CstI i -> i
-  | Plus(e1, e2) -> eval e1 + eval e2
-  | Minus(e1, e2) -> eval e1 - eval e2
-  | Star(e1, e2) -> eval e1 * eval e2
-  | Slash(e1, e2) -> eval e1 / eval e2
+  | Prim(op, e1, e2) ->
+     let (i1, i2) = (eval e1, eval e2) in
+     (match op with
+      | "+" -> i1 + i2
+      | "-" -> i1 - i2
+      | "*" -> i1 * i2
+      | "/" -> i1 / i2
+      | _ -> failwith "Unrecognized operator."
+     )
 
 (* compile: expr -> (stackInstruction list) *)
 let rec compile e =
   match e with
   | CstI i -> [Int i]
-  | Plus(e1, e2)  -> (compile e1)@(compile e2)@[Add]
-  | Minus(e1, e2) -> (compile e1)@(compile e2)@[Subt]
-  | Star(e1, e2)  -> (compile e1)@(compile e2)@[Mult]
-  | Slash(e1, e2) -> (compile e1)@(compile e2)@[Divide]
+  | Prim(op, e1, e2) ->
+     let opCode =
+       match op with
+       | "+" -> Add
+       | "-" -> Subt
+       | "*" -> Mult
+       | "/" -> Divide
+     in (compile e1)@(compile e2)@[opCode]
 
 
 (* 4 + 5 *)                                                 
-let e0 = Plus(CstI 4, CstI 5)
+let e0 = Prim("+", CstI 4, CstI 5)
 
 (* (4+5)-(2*5) *)                                    
-let e1 = Minus(Plus(CstI 4, CstI 5),
-               Star(CstI 2, CstI 5))
+let e1 = Prim("-", Prim("+", CstI 4, CstI 5),
+               Prim("*", CstI 2, CstI 5))
 
 (* 4 - 5 * 2 *)
-let e2 = Minus(CstI 4, Star(CstI 5, CstI 2))
+let e2 = Prim("-", CstI 4, Prim("*", CstI 5, CstI 2))
 
 (* 15 - 6 - 3 - 4 *)
-let e3 = Minus(
-             Minus(
-                 Minus(CstI 15, CstI 6),
+let e3 = Prim("-", 
+             Prim("-", 
+                 Prim("-", CstI 15, CstI 6),
                  CstI 3),
              CstI 4)
 
 (* 15 - (6 - (3 - 4)) *)
-let e4 = Minus(CstI 15,
-               Minus(CstI 6,
-                     Minus(CstI 3, CstI 4)))
+let e4 = Prim("-", CstI 15,
+               Prim("-", CstI 6,
+                     Prim("-", CstI 3, CstI 4)))
 
 
               
