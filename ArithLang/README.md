905d8e3b3e539322486f274466a9c84b98de30a9
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-10-19 13:44:45 +0300

    Extended the language to handle names.

--- ArithLang/README.md
+++ ArithLang/README.md
@@ -8,3 +8,18 @@ This is a very simple programming language
 where we have arithmetic expressions (add, multiply, subtract, divide),
 and names only.
 
+To run:
+
+```
+# #use "arith.ml";;
+...
+# eval e1 [];;
+- : int = 42
+# eval e1 env1;;
+- : int = 42
+# eval e4 env1;;
+- : int = -5
+# eval e4 env2;;
+- : int = -96
+```
+
--- ArithLang/arith.ml
+++ ArithLang/arith.ml
@@ -1,19 +1,29 @@
 type exp = CstI of int
+         | Var of string
          | Add of exp * exp
          | Mult of exp * exp
          | Subt of exp * exp
          | Div of exp * exp
 
+let rec lookup x env =
+  match env with
+  | [] -> failwith ("Unbound name " ^ x)
+  | (y,i)::rest -> if x = y then i
+                   else lookup x rest
 
-(* eval: exp -> int *)                          
-let rec eval e =
+let env1 = [("a", 5); ("b", 8); ("zzz", 13)]
+
+let env2 = ("b", 99)::env1
+
+(* eval: exp -> (string * int) list -> int *)
+let rec eval e env =
   match e with
   | CstI i -> i
-  | Add(e1, e2)  -> eval e1 + eval e2
-  | Mult(e1, e2) -> eval e1 * eval e2
-  | Subt(e1, e2) -> eval e1 - eval e2
-  | Div(e1, e2)  -> eval e1 / eval e2
-
+  | Var x -> lookup x env
+  | Add(e1, e2)  -> eval e1 env + eval e2 env
+  | Mult(e1, e2) -> eval e1 env * eval e2 env
+  | Subt(e1, e2) -> eval e1 env - eval e2 env
+  | Div(e1, e2)  -> eval e1 env / eval e2 env
   
 (* 30 + 6 * 2 *)
 let e1 = Add(CstI(30),
@@ -30,4 +40,9 @@ let e3 = Div(Mult(CstI(5), CstI(2)),
 (* 5 - 2 - 3 *)
 let e4 = Subt(Subt(CstI(5), CstI(2)),
               CstI(3))
+
+(* a - 2 - b *)
+let e4 = Subt(Subt(Var("a"),
+                   CstI(2)),
+              Var("b"))
               
