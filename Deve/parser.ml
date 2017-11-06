f76fba352c8e7e2ed6e62572b92818dbf4e118ce
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-11-06 10:41:36 +0300

    Wrote an explicit parseMain function.

--- Deve/parser.ml
+++ Deve/parser.ml
@@ -1,5 +1,5 @@
 (* parseExp: token list -> (exp, token list) 
-   Parses an exp out of the iven token list,
+   Parses an exp out of the given token list,
    returns that exp together with the unconsumed tokens.
  *)
 let parseExp tokens =
@@ -7,9 +7,12 @@ let parseExp tokens =
   | INT i :: rest -> (CstI i, rest)
   | NAME x :: rest -> (Var x, rest)
 
-(* parse: string -> exp *)
-let rec parse s =
-  let tokens = scan s in
+(* parseMain: token list -> exp *)
+let parseMain tokens =
   match parseExp tokens with
   | (e, [EOF]) -> e
   | _ -> failwith "I was expecting to see an EOF."
+
+(* parse: string -> exp *)
+let rec parse s =
+  parseMain (scan s)
