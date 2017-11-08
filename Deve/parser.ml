cf516d520d7676cfb2a89f51c0693141b18d7b21
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-11-08 15:28:30 +0300

    Refactoring for a cleaner code.

--- Deve/parser.ml
+++ Deve/parser.ml
@@ -1,3 +1,32 @@
+(* A helper function to convert a token to a string *)
+let toString tok =
+  match tok with
+  | INT i -> "INT(" ^ string_of_int i ^ ")"
+  | BOOL b -> "BOOL(" ^ string_of_bool b ^ ")"
+  | NAME x -> "NAME(\"" ^ x ^ "\")"
+  | PLUS -> "PLUS"
+  | STAR -> "STAR"
+  | MINUS -> "MINUS"
+  | SLASH -> "SLASH"
+  | LET -> "LET"
+  | EQUALS -> "EQUALS"
+  | IN -> "IN"
+  | IF -> "IF"
+  | THEN -> "THEN"
+  | ELSE -> "ELSE"
+  | ERROR c -> "ERROR('" ^ (charToString c) ^ "')"
+  | EOF -> "EOF"
+
+(* consume: token -> token list -> token list
+   Enforces that the given token list's head is the given token;
+   returns the tail.
+*)
+let consume tok tokens =
+  match tokens with
+  | t::rest when t = tok -> rest
+  | t::rest -> failwith ("I was expecting a " ^ (toString tok) ^
+                         ", but I found a " ^ toString(t))
+
 (* parseExp: token list -> (exp, token list) 
    Parses an exp out of the given token list,
    returns that exp together with the unconsumed tokens.
@@ -8,27 +37,25 @@ let rec parseExp tokens =
   | BOOL b :: rest -> (CstB b, rest)
   | NAME x :: rest -> (Var x, rest)
   | LET::(NAME x)::EQUALS::rest ->
-     (match parseExp rest with
-      | (e1, IN::rest1) -> let (e2, rest2) = parseExp rest1
-                           in (LetIn(x, e1, e2), rest2)
-      | (e1, _) -> failwith "I was expecting to see an IN."
-     )
+     let (e1, tokens1) = parseExp rest in
+     let tokens2 = consume IN tokens1 in
+     let (e2, tokens3) = parseExp tokens2 in
+     (LetIn(x, e1, e2), tokens3)
   | IF::rest ->
-     (match parseExp rest with
-      | (e1, THEN::rest1) ->
-         (match parseExp rest1 with
-          | (e2, ELSE::rest2) -> let (e3, rest3) = parseExp rest2
-                                 in (If(e1, e2, e3), rest3)
-          | (e2, _) -> failwith "I was expecting to see an ELSE."
-         )
-      | (e1, _) -> failwith "I was expecting to see a THEN."
-     )
+     let (e1, tokens1) = parseExp rest in
+     let tokens2 = consume THEN tokens1 in
+     let (e2, tokens3) = parseExp tokens2 in
+     let tokens4 = consume ELSE tokens3 in
+     let (e3, tokens5) = parseExp tokens4 in
+     (If(e1, e2, e3), tokens5)
+  | t::rest -> failwith ("Unsupported token: " ^ toString(t))
 
 (* parseMain: token list -> exp *)
 let parseMain tokens =
-  match parseExp tokens with
-  | (e, [EOF]) -> e
-  | _ -> failwith "I was expecting to see an EOF."
+  let (e, tokens1) = parseExp tokens in
+  let tokens2 = consume EOF tokens1 in
+  if tokens2 = [] then e
+  else failwith "Oops."
 
 (* parse: string -> exp *)
 let rec parse s =
