ab830c54d18d9abb5ba00492372f3ba780ee7cf8
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-11-08 19:45:31 +0300

    A version that can parse long binary expressions, but right associatively.

--- Deve/parser.ml
+++ Deve/parser.ml
@@ -61,23 +61,47 @@ and parseIfThenElse tokens =
 and parseLevel2Exp tokens =
   let (e1, tokens1) = parseLevel3Exp tokens in
   match tokens1 with
-  | PLUS::rest ->
-     let (e2, tokens2) = parseLevel3Exp rest
-     in (Add(e1, e2), tokens2)
-  | MINUS::rest ->
-     let (e2, tokens2) = parseLevel3Exp rest
-     in (Subt(e1, e2), tokens2)
+  | PLUS::tok::rest ->
+     (match tok with
+      | LET -> let (e2, tokens2) = parseLetIn (tok::rest)
+               in (Add(e1, e2), tokens2)
+      | IF  -> let (e2, tokens2) = parseIfThenElse (tok::rest)
+               in (Add(e1, e2), tokens2)
+      | t   -> let (e2, tokens2) = parseLevel2Exp (tok::rest)
+               in (Add(e1, e2), tokens2)
+     )
+  | MINUS::tok::rest ->
+     (match tok with
+      | LET -> let (e2, tokens2) = parseLetIn (tok::rest)
+               in (Subt(e1, e2), tokens2)
+      | IF  -> let (e2, tokens2) = parseIfThenElse (tok::rest)
+               in (Subt(e1, e2), tokens2)
+      | t   -> let (e2, tokens2) = parseLevel2Exp (tok::rest)
+               in (Subt(e1, e2), tokens2)
+     )
   | _ -> (e1, tokens1)
      
 and parseLevel3Exp tokens =
   let (e1, tokens1) = parseLevel4Exp tokens in
   match tokens1 with
-  | STAR::rest ->
-     let (e2, tokens2) = parseLevel4Exp rest
-     in (Mult(e1, e2), tokens2)
-  | SLASH::rest ->
-     let (e2, tokens2) = parseLevel4Exp rest
-     in (Div(e1, e2), tokens2)
+  | STAR::tok::rest ->
+     (match tok with
+      | LET -> let (e2, tokens2) = parseLetIn (tok::rest)
+               in (Mult(e1, e2), tokens2)
+      | IF  -> let (e2, tokens2) = parseIfThenElse (tok::rest)
+               in (Mult(e1, e2), tokens2)
+      | t   -> let (e2, tokens2) = parseLevel3Exp (tok::rest)
+               in (Mult(e1, e2), tokens2)
+     )
+  | SLASH::tok::rest ->
+     (match tok with
+      | LET -> let (e2, tokens2) = parseLetIn (tok::rest)
+               in (Div(e1, e2), tokens2)
+      | IF  -> let (e2, tokens2) = parseIfThenElse (tok::rest)
+               in (Div(e1, e2), tokens2)
+      | t   -> let (e2, tokens2) = parseLevel3Exp (tok::rest)
+               in (Div(e1, e2), tokens2)
+     )
   | _ -> (e1, tokens1)
 
 and parseLevel4Exp tokens =
