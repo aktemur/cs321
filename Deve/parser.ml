edd7fffe58cea6777ee0ede8674cd968e7a8d610
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-11-08 19:35:21 +0300

    A limited version that can parse simple binary expressions.

--- Deve/parser.ml
+++ Deve/parser.ml
@@ -32,22 +32,59 @@ let consume tok tokens =
    returns that exp together with the unconsumed tokens.
  *)
 let rec parseExp tokens =
+  parseLevel1Exp tokens
+
+and parseLevel1Exp tokens =
   match tokens with
-  | INT i :: rest -> (CstI i, rest)
-  | BOOL b :: rest -> (CstB b, rest)
-  | NAME x :: rest -> (Var x, rest)
-  | LET::(NAME x)::EQUALS::rest ->
+  | LET::rest -> parseLetIn tokens
+  | IF::rest -> parseIfThenElse tokens
+  | _ -> parseLevel2Exp tokens
+
+and parseLetIn tokens =
+  match tokens with
+  | LET::NAME(x)::EQUALS::rest ->
      let (e1, tokens1) = parseExp rest in
      let tokens2 = consume IN tokens1 in
      let (e2, tokens3) = parseExp tokens2 in
      (LetIn(x, e1, e2), tokens3)
-  | IF::rest ->
-     let (e1, tokens1) = parseExp rest in
-     let tokens2 = consume THEN tokens1 in
-     let (e2, tokens3) = parseExp tokens2 in
-     let tokens4 = consume ELSE tokens3 in
-     let (e3, tokens5) = parseExp tokens4 in
-     (If(e1, e2, e3), tokens5)
+  | _ -> failwith "Should not be possible."
+
+and parseIfThenElse tokens =
+  let rest = consume IF tokens in
+  let (e1, tokens1) = parseExp rest in
+  let tokens2 = consume THEN tokens1 in
+  let (e2, tokens3) = parseExp tokens2 in
+  let tokens4 = consume ELSE tokens3 in
+  let (e3, tokens5) = parseExp tokens4 in
+  (If(e1, e2, e3), tokens5)
+  
+and parseLevel2Exp tokens =
+  let (e1, tokens1) = parseLevel3Exp tokens in
+  match tokens1 with
+  | PLUS::rest ->
+     let (e2, tokens2) = parseLevel3Exp rest
+     in (Add(e1, e2), tokens2)
+  | MINUS::rest ->
+     let (e2, tokens2) = parseLevel3Exp rest
+     in (Subt(e1, e2), tokens2)
+  | _ -> (e1, tokens1)
+     
+and parseLevel3Exp tokens =
+  let (e1, tokens1) = parseLevel4Exp tokens in
+  match tokens1 with
+  | STAR::rest ->
+     let (e2, tokens2) = parseLevel4Exp rest
+     in (Mult(e1, e2), tokens2)
+  | SLASH::rest ->
+     let (e2, tokens2) = parseLevel4Exp rest
+     in (Div(e1, e2), tokens2)
+  | _ -> (e1, tokens1)
+
+and parseLevel4Exp tokens =
+  match tokens with
+  | INT i :: rest -> (CstI i, rest)
+  | NAME x :: rest -> (Var x, rest)
+  | BOOL b :: rest -> (CstB b, rest)
   | t::rest -> failwith ("Unsupported token: " ^ toString(t))
 
 (* parseMain: token list -> exp *)
