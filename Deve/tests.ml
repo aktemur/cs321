0aacb908482d26e40ed77c83d9592a361849dff3
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-11-09 14:11:53 +0300

    Test cases.

--- Deve/tests.ml
+++ Deve/tests.ml
@@ -14,15 +14,15 @@ assert (run "let a = 3
                    in 7 * a - 9 / b + c" = 23);;
 assert (run "(let c = 3 in c + c) + (let c = 5 in c * c)" = 31);;
 assert (run "let a = 5
-             in (let b = 3 in a + b) + (let b = 9 in a * b)" = 48);;
+             in (let b = 3 in a + b) + (let b = 9 in a * b)" = 53);;
 assert (run "let x =
                let a = 5
                in let b = 8
                   in a + b
              in x * 2" = 26);;
+assert (run "if true then 42 else 8" = 42);;
+assert (run "if false then 42 else 8" = 8);;
 assert (run "let x = 4
              in if x + 1 >= 0 then 42 else x * 8" = 42);;
 assert (run "let x = 4
              in if x < 4 then 42 else x + 8" = 12);;
-assert (run "if true then 42 else 8" = 42);;
-assert (run "if false then 42 else 8" = 8);;
