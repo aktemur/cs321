acd8eb5eea3a7679551997071925ec1f8083f610
Baris Aktemur <baris.aktemur@ozyegin.edu.tr>
2017-11-09 10:44:04 +0300

    REPL for Deve.

--- Deve/main.ml
+++ Deve/main.ml
@@ -2,7 +2,24 @@
 #use "lexer.ml";;
 #use "parser.ml";;
 
+open Printf
+
 let run code =
   eval (parse code) []
 ;;
 
+let deveREPL() =
+  let rec readInput() =
+    let s = String.trim(read_line()) in
+    let len = String.length s in
+    if len >= 2 && (String.sub s (len - 2) 2) = ";;"
+    then (String.sub s 0 (len - 2))
+    else s ^ "\n" ^ readInput()
+  in
+  let rec loop() =
+    print_string "D> ";
+    let s = readInput() in
+    let v = run s in
+    printf "val: %d\n" v;
+    loop()
+  in loop()      
